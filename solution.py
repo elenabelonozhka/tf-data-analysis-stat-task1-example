da4f498bab92efa714788777567f43d7f0cf76d1
elenabelonozhka <64587763+elenabelonozhka@users.noreply.github.com>
2023-04-01 16:14:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import expon
 
 chat_id = 728846853 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    n=len(x)
+    y = expon.rvs(size = [n])
+    x = x - 15 + y
     return x.mean() # Ваш ответ
