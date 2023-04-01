7901828bb9efbf0047ab308a69ebebf6dc484a94
elenabelonozhka <64587763+elenabelonozhka@users.noreply.github.com>
2023-04-01 16:15:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,9 @@ from scipy.stats import expon
 chat_id = 728846853 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
+    t = 10
     n=len(x)
     y = expon.rvs(size = [n])
     x = x - 15 + y
+    x = x/t
     return x.mean() # Ваш ответ
